a4fc03d780635157d38f3be2e3dbc7e5ded25a27
William Lees <william@lees.org.uk>
2018-04-04 14:17:00 +0100

    Remove duplicate biobox-example files: will use submodule instead.

--- biobox-example/example/parse_bb.sh
+++
@@ -1,7 +0,0 @@
-docker run -i -t \
-  --volume="$(pwd)/input_data:/bbx/mnt/input:ro" \
-  --volume="$(pwd)/output_data:/bbx/mnt/output:rw" \
-  --volume="$(pwd)/cache:/bbx/mnt/cache:rw" \
-  --rm \
-  williamlees/igblast \
-  parse
